c57ffc30b009d8612e7d61410b3f38d92f3bbcd9
يسوع المسيح <165551623+KiRiLldrs@users.noreply.github.com>
2025-03-25 18:37:44 +0400

    Обработка исключений

--- task2/main.py
+++ task2/main.py
@@ -7,25 +7,31 @@ def create_parser()->tuple:
     parser = argparse.ArgumentParser()
     parser.add_argument("cpp_sequence", type=str, help="path to file with sequence, created in c++")
     parser.add_argument("java_sequence", type=str, help="path to file with sequence, created in java")
+    parser.add_argument("result_file", type=str, help="path to file where result will be written")
     args=parser.parse_args()
-    return args.cpp_sequence, args.java_sequence
+    return args.cpp_sequence, args.java_sequence, args.result_file
 
 
 def main():
-    cpp_sequence = NIST.open_file(create_parser()[0])
-    java_sequence = NIST.open_file(create_parser()[1])
+    try:
+        cpp_sequence = NIST.open_file(create_parser()[0])
+        java_sequence = NIST.open_file(create_parser()[1])
+        result_file = create_parser()[2]
 
-    print("Frequency bitwise test:")
-    print("cpp: ", NIST.frequency_test(cpp_sequence))
-    print("java: ", NIST.frequency_test(java_sequence))
+        NIST.write_file("Frequency bitwise test:\n", result_file)
+        NIST.write_file("cpp: " + str(NIST.frequency_test(cpp_sequence)) + "\n", result_file)
+        NIST.write_file("java: " + str(NIST.frequency_test(java_sequence)) + "\n", result_file)
 
-    print("The consecutive identical bits test:")
-    print("cpp: ", NIST.consecutive_identical_test(cpp_sequence))
-    print("java: ", NIST.consecutive_identical_test(java_sequence))
+        NIST.write_file("The consecutive identical bits test:\n", result_file)
+        NIST.write_file("cpp: " + str(NIST.consecutive_identical_test(cpp_sequence)) + "\n", result_file)
+        NIST.write_file("java: " + str(NIST.consecutive_identical_test(java_sequence)) + "\n", result_file)
 
-    print("The longest sequence of units in block test:")
-    print("cpp: ", NIST.longest_sequence_in_block_test(NIST.block_statistic(cpp_sequence)))
-    print("java: ", NIST.longest_sequence_in_block_test(NIST.block_statistic(java_sequence)))
+        NIST.write_file("The longest sequence of units in block test:\n", result_file)
+        NIST.write_file("cpp: " + str(NIST.longest_sequence_in_block_test(NIST.block_statistic(cpp_sequence))) + "\n", result_file)
+        NIST.write_file("java: " + str(NIST.longest_sequence_in_block_test(NIST.block_statistic(java_sequence))) + "\n", result_file)
+
+    except Exception as e:
+        print(f"An error occurred while accessing the directory: {e} ")
 
 
 if __name__ == "__main__":
